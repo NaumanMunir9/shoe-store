580a1c8d7e1a12aef291c060cdcfe28be912ff7e
Nauman Munir <naumanmunir@gmail.com>
2020-07-15 01:24:37 +0500

    Product Page updated with shoes and grid css

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Shoes from "./../shoes.json";
 
 const Product = () => {
   return (
     <div>
       <h1>Hello from Product</h1>
+      <div className="productContainer">
+        {Object.keys(Shoes).map((keyName) => {
+          const shoe = Shoes[keyName];
+          return (
+            <div key={keyName}>
+              <h2>{shoe.name}</h2>
+              <img src={shoe.img} alt="shoe-pair" height={150} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
